78a06e77bd1b1b4b0e67247a4c88adeacaf34958
Nicole Guittari <nicole.guittari@jhuapl.edu>
2025-03-11 11:20:07 -0400

    Update hrv.py

--- src/spec/hrv.py
+++ src/spec/hrv.py
@@ -1,21 +1,21 @@
 from pynwb.spec import NWBGroupSpec, NWBDatasetSpec
 
- from datetime import datetime
- from dateutil.tz import tzlocal
- import numpy as np
+from datetime import datetime
+from dateutil.tz import tzlocal
+import numpy as np
 
- def make_hrv_stage():
-     hrv_series = NWBGroupSpec(
-         doc='Stores HRV values as strings over time.',
-         datasets=[
-             NWBDatasetSpec(
-                 name='data',
-                 dtype='text',
-                 doc='HRV calculated values'
-             )
-         ],
-         neurodata_type_def='HRVSeries',
-         neurodata_type_inc='TimeSeries',
-     )
+def make_hrv_stage():
+    hrv_series = NWBGroupSpec(
+        doc='Stores HRV values as strings over time.',
+        datasets=[
+            NWBDatasetSpec(
+                name='data',
+                dtype='float',
+                doc='HRV calculated values'
+            )
+        ],
+        neurodata_type_def='HRVSeries',
+        neurodata_type_inc='TimeSeries',
+    )
 
-     return hrv_series 
+    return hrv_series 
