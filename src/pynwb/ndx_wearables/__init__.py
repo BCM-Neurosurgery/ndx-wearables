fc162a1341fef64062c9613039f7ed3f3fd05dba
Lauren Diaz <lauren.diaz@jhuapl.edu>
2025-04-08 13:20:48 -0400

    slight fixes from merge

--- src/pynwb/ndx_wearables/__init__.py
+++ src/pynwb/ndx_wearables/__init__.py
@@ -22,8 +22,7 @@ load_namespaces(str(__spec_path))
 # Either have PyNWB generate a class from the spec using `get_class` as shown
 # below or write a custom class and register it using the class decorator
 # `@register_class("TetrodeSeries", "ndx-wearables")`
-
-WearableSensor = get_class("WearableSensor", "ndx-wearables")
+from .wearables_classes import *
 WearableTimeSeries = get_class("WearableTimeSeries", "ndx-wearables")
 
 
