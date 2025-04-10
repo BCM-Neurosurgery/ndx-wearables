6095d0798e63c5c121b8abca525e51f56f9d7698
Lauren Diaz <lauren.diaz@jhuapl.edu>
2025-04-10 14:26:56 -0400

    cleanup unnecessary attributes + comments; adjust path for test nwb

--- src/pynwb/tests/test_wearables_infrastructure.py
+++ src/pynwb/tests/test_wearables_infrastructure.py
@@ -10,7 +10,7 @@ from ndx_wearables import WearableDevice, WearableTimeSeries
 
 @pytest.fixture
 def tmp_path():
-    return Path('../../../')
+    return Path('./src/pynwb/tests')
 
 @pytest.fixture
 def nwb_with_wearables_data(tmp_path):
