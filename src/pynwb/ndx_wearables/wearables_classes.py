93480e1ceb7af5251df98ac3acc5ad3dd228e24d
TomekFraczek <tfraczek@uw.edu>
2025-05-09 13:02:51 -0500

    HDMF does not support multiple inheritance, so we do weird stuff

--- src/pynwb/ndx_wearables/wearables_classes.py
+++ src/pynwb/ndx_wearables/wearables_classes.py
@@ -38,14 +38,30 @@ class WearableDevice(Device):
         self.location = location
 
 class WearableBase(NWBContainer):
-    @docval(
-            {'name': 'wearable_device', 'type': 'WearableDevice', 'doc': 'Link to the WearableDevice used to record the data'}
+    """
+    HDMF and by extension NWB does not really support multiple inheritance.
+    As a result, this class is "invisibly" inherited as a mixin
+
+    For this to work properly, the inheriting class (at the time of writing, WearableTimeSeries and WearableEvents)
+    must append the result of get_wearables_docval() to the docval of the init method, and call the function
+    wearables_init_helper in the init method.
+    """
+
+    @staticmethod
+    def get_wearables_docval():
+        return (
+            {
+                'name': 'wearable_device',
+                'type': 'WearableDevice',
+                'doc': 'Link to the WearableDevice used to record the data'
+            },
         )
 
-    def __init__(self, **kwargs):
+    def wearables_init_helper(self, **kwargs):
+        print('In WearableBase init')
         wearable_device = popargs('wearable_device', kwargs)
-        super().__init__(**kwargs)
         self.wearable_device = wearable_device
+        return kwargs
 
 
 # # Adding events to inherit from ndx-wearables:
@@ -63,7 +79,9 @@ class WearableBase(NWBContainer):
 
 @register_class("WearableTimeSeries", "ndx-wearables")
 class WearableTimeSeries(WearableBase, TimeSeries):
-    @docval(*(get_docval(TimeSeries.__init__) + get_docval(WearableBase.__init__)))
 
+    @docval(*(get_docval(TimeSeries.__init__) + WearableBase.get_wearables_docval()))
     def __init__(self, **kwargs):
+        print('In WearableTimeSeries init')
+        kwargs = self.wearables_init_helper(**kwargs)
         super().__init__(**kwargs)
