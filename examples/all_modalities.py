f1271c8da7849a41b86b50d431acb486fa43a5af
TomekFraczek <tfraczek@uw.edu>
2025-05-12 16:01:01 -0500

    Working demo of the all_modalities.py script, at least with the generic base classes

---
+++ examples/all_modalities.py
@@ -0,0 +1,27 @@
+from pynwb import NWBHDF5IO
+
+from tests import make_wearables_nwbfile, add_wearables_device
+from tests.test_wearables_infrastructure import add_wearable_events, add_wearable_timeseries
+
+# List of all modality specific build functions. Add your mode functions here to register them in the full demo NWB
+# Each of these functions should take a pre-built NWB file, and add some synthetic data for the modality in question
+MODALITY_BUILDERS = [
+    add_wearable_events,
+    add_wearable_timeseries,    # TODO: Remove generic types once more interesting test data is available
+]
+
+OUTPUT_PATH = './all_wearable_modalities.nwb'
+
+
+def main():
+    ready_nwb, device = add_wearables_device(make_wearables_nwbfile())
+
+    for mode_build in MODALITY_BUILDERS:
+        ready_nwb = mode_build(ready_nwb, device)
+
+    with NWBHDF5IO(OUTPUT_PATH, mode='w') as io:
+        io.write(ready_nwb)
+
+
+if __name__ == "__main__":
+    main()
