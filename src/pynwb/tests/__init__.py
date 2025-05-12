fe894aeb93417e923473259b4b7480410ee9be30
TomekFraczek <tfraczek@uw.edu>
2025-05-12 15:54:03 -0500

    Pull out functionality of pytest fixtures since they can't be used as regular functions

--- src/pynwb/tests/__init__.py
+++ src/pynwb/tests/__init__.py
@@ -7,13 +7,7 @@ from ndx_events import NdxEventsNWBFile, MeaningsTable, CategoricalVectorData
 from ndx_wearables import WearableDevice, WearableTimeSeries, WearableEvents
 
 
-
-@pytest.fixture
-def tmp_path():
-    return Path('./src/pynwb/tests')
-
-@pytest.fixture
-def wearables_nwbfile():
+def make_wearables_nwbfile():
     nwbfile = NdxEventsNWBFile(
         session_description="Example wearables study session",
         identifier='TEST_WEARABLES',
@@ -22,18 +16,29 @@ def wearables_nwbfile():
 
     # create processing module
     wearables_module = ProcessingModule(
-        name = "wearables",
-        description = "Wearables data",
+        name="wearables",
+        description="Wearables data",
     )
 
     nwbfile.add_processing_module(wearables_module)
-    return wearables_module, nwbfile
+    return nwbfile
 
-@pytest.fixture
-def wearables_nwbfile_device(wearables_nwbfile):
+def add_wearables_device(nwbfile):
     # create wearables device
-    wearables_module, nwbfile = wearables_nwbfile
     device = WearableDevice(name="test_wearable_device", description="test", location="arm", manufacturer="test")
     nwbfile.add_device(device)
 
     return nwbfile, device
+
+@pytest.fixture
+def tmp_path():
+    return Path('./src/pynwb/tests')
+
+@pytest.fixture
+def wearables_nwbfile():
+    return make_wearables_nwbfile()
+
+@pytest.fixture
+def wearables_nwbfile_device(wearables_nwbfile):
+    wearables_module, nwbfile = wearables_nwbfile
+    return add_wearables_device(nwbfile)
