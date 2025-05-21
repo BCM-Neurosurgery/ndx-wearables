45d8ce83bdd34bebc124b171ac55fa23d9772b73
TomekFraczek <tfraczek@uw.edu>
2025-05-21 11:57:09 -0500

    Remove attempts at class registration

--- src/spec/hrv.py
+++ src/spec/hrv.py
@@ -1,14 +1,5 @@
 from pynwb.spec import NWBGroupSpec, NWBDatasetSpec
-from datetime import datetime
-from dateutil.tz import tzlocal
-import numpy as np
-from pynwb import register_class
-from ndx_wearables import WearableTimeSeries
 
-@register_class('HRVSeries', 'ndx-wearables')
-class HRVSeries(WearableTimeSeries):
-    """HRV data stored as a wearable time series"""
-    pass
 
 def make_hrv_stage():
     hrv_series = NWBGroupSpec(
@@ -19,6 +10,4 @@ def make_hrv_stage():
 
     return hrv_series 
 
-from ndx_wearables import WearableTimeSeries
-from pynwb import register_class
 
