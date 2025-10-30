351517e1bbc320394852c304009dd6a61867093b
Nicole Guittari <nicole.guittari@jhuapl.edu>
2025-10-30 14:02:10 -0400

    Removing PyTest mention

--- examples/conftest.py
+++ examples/conftest.py
@@ -2,17 +2,14 @@
 from pathlib import Path
 from examples import make_wearables_nwbfile, add_wearables_device
 
-@pytest.fixture(scope='session')
 def tmp_path():
     return Path('./examples/test_nwb_file.nwb')
     #return Path('./src/pynwb/tests/test_nwb_file.nwb')
 
-@pytest.fixture(scope='session')
 def wearables_nwbfile():
     nwbfile = make_wearables_nwbfile()
     return nwbfile
 
-@pytest.fixture(scope='session')
 def wearables_nwbfile_device(wearables_nwbfile):
     nwbfile = wearables_nwbfile
     nwbfile, device = add_wearables_device(nwbfile)
